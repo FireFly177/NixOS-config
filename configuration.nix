c2cf1bdf7f2ae006d3853fbd229e11913d00bbb3
DuyNguyen <huonghaiduynhim@gmail.com>
2024-09-14 04:14:26 +0700

    Change hostname to "nixos"

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,7 @@
   # Flake support
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  networking.hostName = "nixoslaptop"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
