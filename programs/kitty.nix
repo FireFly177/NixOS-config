4551860eee132497a9b62b0bbf7647cf67d33b8f
DuyNguyen <huonghaiduynhim@gmail.com>
2024-09-15 19:50:00 +0700

    Refactor kitty.nix configuration

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -4,27 +4,28 @@
   programs.kitty = {
     enable = true;  # Enable Kitty terminal emulator
 
-    # Kitty settings written to $XDG_CONFIG_HOME/kitty/kitty.conf
-    settings = {
-        # Font settings
-        font_family = "JetBrainsMono Nerd Font Mono";
-        font_size = 13;
+    extraConfig = ''
+      # Font settings
+      font_family JetBrainsMono Nerd Font Mono
+      font_size 13
 
-        scrollback_lines = 5000;
+      # Scrollback settings
+      scrollback_lines 5000
 
-        enable_ligatures = "always";
-        mouse_hide_wait 3.0
+      # Ligatures
+      enable_ligatures always
+      mouse_hide_wait 3.0
 
-        tab_bar_edge "top";
-        tab_bar_style "fade";
-        tab_bar_min_tab 1;
+      # Tab bar settings
+      tab_bar_edge top
+      tab_bar_style fade
+      tab_bar_min_tabs 1
 
-        background_opacity 0.8;
-        # Set window padding
-        window_padding_width = 5;
-        window_border_width = 2;
-
-    };
+      # Window settings
+      background_opacity 0.8
+      window_padding_width 5
+      window_border_width 2
+    '';
 
     # enableWayland = true;
   };
